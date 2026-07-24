59a7ecb15c0612b74c846c56be1c47ce135452da
Usama Munir <hello@usamamuneer.me>
2026-07-24 21:16:52 +0500

    init: registered `Display` blade component

---
+++ src/View/Components/Display.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Usamamuneerchaudhary\Adment\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\HtmlString;
+use Illuminate\View\Component;
+use Illuminate\View\ComponentAttributeBag;
+use Usamamuneerchaudhary\Adment\Contracts\ManagesAds;
+
+/**
+ * <x-adment::display location="sidebar" />
+ * <x-adment::display location="sidebar" :single="false" />
+ * <x-adment::display ad-key="HOMEPAGEBNNR" />
+ */
+class Display extends Component
+{
+    /** Create a display component for a location or a specific ad key. */
+    public function __construct(
+        protected ManagesAds $ads,
+        public ?string $location = null,
+        public ?string $adKey = null,
+        public bool $single = true,
+    ) {}
+
+    /** Render the resolved ad HTML into the display Blade view. */
+    public function render(): Closure
+    {
+        return function (array $data): View {
+            /** @var ComponentAttributeBag $attributes */
+            $attributes = $data['attributes'] ?? new ComponentAttributeBag([]);
+
+            $extra = collect($attributes->getAttributes())
+                ->except(['location', 'ad-key', 'adKey', 'single'])
+                ->map(fn ($value): string => (string) $value)
+                ->all();
+
+            $html = $this->adKey !== null
+                ? ($this->ads->displayAds($this->adKey, $extra) ?? '')
+                : ($this->location !== null ? $this->ads->display($this->location, $extra, $this->single) : '');
+
+            return view('adment::components.display', [
+                'html' => new HtmlString($html),
+            ]);
+        };
+    }
+}
